95b0e2fef6f347efbea209f9536cea7dd63e18d5
Chris L Chapman <ChristopherLChapman42@gmail.com>
2018-06-23 22:39:02 -0700

    delete and update book

--- server.js
+++ server.js
@@ -16,17 +16,19 @@ client.on('error', err => console.error(err));
 
 // Application Middleware
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
 
 // API Endpoints
 app.get('/api/v1/books/:id', (req, res) => {
-  console.log('You want just ONE book!'); 
-  // res.send('Here is your 1 book!'); 
-  let SQL = `SELECT * FROM books 
+  console.log('You want just ONE book!');
+  // res.send('Here is your 1 book!');
+  let SQL = `SELECT * FROM books
               WHERE book_id = ${req.params.id};`;
   client.query(SQL)
     .then(results => res.send(results.rows))
-    .catch(console.error); 
-}); 
+    .catch(console.error);
+});
 
 app.get('/api/v1/books', (req, res) => {
   console.log('OMG I have done been visited by a client!!!!!');
@@ -36,14 +38,64 @@ app.get('/api/v1/books', (req, res) => {
     .catch(console.error);
 });
 
+app.post('/api/v1/books', (req, res) => {
+  console.log('req body', req.body);
 
-// app.delete('/articles/:id', (request, response) => {
-//   console.log('You burn books now?'); 
-//   let SQL = `DELETE FROM art'icles WHERE article_id=$1;`;
-//   let values = [request.params.id];
+  let SQL = `
+  INSERT INTO books(title, author, isbn, image_url, description)
+  VALUES ($1, $2, $3, $4, $5);
+  `;
+  let values = [
+    req.body.title,
+    req.body.author,
+    req.body.isbn,
+    req.body.image_url,
+    req.body.description
+  ];
 
-//   client.query(SQL, values)
-//     .then(() => {
+  client.query(SQL, values).then(function() {
+    res.send('insert complete')
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+});
+
+app.put('/api/v1/books/:id', (req, res) => {
+  console.log('Update on req.body', req.body);
+
+  let SQL = `
+  UPDATE books SET title=$1, author=$2, isbn=$3, image_url=$4, description=$5
+  WHERE book_id=$6; 
+  `;
+  let values = [
+    req.body.title,
+    req.body.author,
+    req.body.isbn,
+    req.body.image_url,
+    req.body.description,
+    req.params.id
+  ];
+
+  // res.send('you updating it boss?'); 
+  client.query(SQL, values).then(function() {
+    res.send('Update complete')
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+});
+
+app.delete('/api/v1/books/:id', (req, res) => {
+  console.log('You burn books now?');
+  // res.send('The horror of deleting a book!'); 
+  let SQL = `DELETE FROM books WHERE book_id=$1;`;
+  let values = [req.params.id];
+
+  client.query(SQL, values)
+    .then(() => res.send('Delete Complete'))
+    .catch(err => console.error(err));  
+}); 
 
 app.get('*', (req, res) => res.status(404).send('This route does not exist'));
 
